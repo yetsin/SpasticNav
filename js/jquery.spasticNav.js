68832b900163e7ee2b2eb0b1cdf63a2ca19bde87
Jeffrey Way <jeffrey@jeffrey-way.com>
2010-03-13 14:18:13 -0600

    Minor JS improvement.

--- js/jquery.spasticNav.js
+++ js/jquery.spasticNav.js
@@ -1,63 +1,59 @@
 (function($) {
 
 	$.fn.spasticNav = function(options) {
-
-        options = $.extend({
-            overlap : 20,
-            speed : 500,
-            reset : 4000,
-            color : '#0b2b61',
-            easing : 'easeOutExpo'
-        }, options);
-
+	
+		options = $.extend({
+			overlap : 20,
+			speed : 500,
+			reset : 1500,
+			color : '#0b2b61',
+			easing : 'easeOutExpo'
+		}, options);
+	
 		return this.each(function() {
-            var nav = $(this),
-                currentPageItem = $('#selected', nav),
-                blob,
-                reset;
-
-            $('<li id="blob"></li>').css({
-                width : currentPageItem.outerWidth(),
-                height : currentPageItem.outerHeight() + options.overlap,
-                left : currentPageItem.position().left,
-                top : currentPageItem.position().top - options.overlap / 2,
-                backgroundColor : options.color
-            }).appendTo('#nav');
-
-            blob = $('#blob');
-
-            $('li', nav).hover(function() {
-                clearTimeout(reset);
-                blob.animate(
-                    {
-                        left : $(this).position().left,
-                        width : $(this).width()
-                    },
-                    {
-                        duration: options.speed,
-                        easing : options.easing,
-                        queue : false
-                    }
-                );
-
-            }, function(e) {
-                // mouse out
-
-                blob.stop().animate({
-                        left : $(this).position().left,
-                        width : $(this).width()
-                    }, options.speed);
-
-                reset = setTimeout(function() {
-                    blob.animate({
-                            width : currentPageItem.outerWidth(),
-                            left: currentPageItem.position().left
-                        }, options.speed);
-                }, options.reset);
-            });
-
+		
+		 	var nav = $(this),
+		 		currentPageItem = $('#selected', nav),
+		 		blob,
+		 		reset;
+		 		
+		 	$('<li id="blob"></li>').css({
+		 		width : currentPageItem.outerWidth(),
+		 		height : currentPageItem.outerHeight() + options.overlap,
+		 		left : currentPageItem.position().left,
+		 		top : currentPageItem.position().top - options.overlap / 2,
+		 		backgroundColor : options.color
+		 	}).appendTo(this);
+		 	
+		 	blob = $('#blob', nav);
+		 	
+			$('li:not(#blob)', nav).hover(function() {
+				// mouse over
+				clearTimeout(reset);
+				blob.animate(
+					{
+						left : $(this).position().left,
+						width : $(this).width()
+					},
+					{
+						duration : options.speed,
+						easing : options.easing,
+						queue : false
+					}
+				);
+			}, function() {
+				// mouse out	
+				reset = setTimeout(function() {
+					blob.animate({
+						width : currentPageItem.outerWidth(),
+						left : currentPageItem.position().left
+					}, options.speed)
+				}, options.reset);
+	
+			});
+		
 		}); // end each
-
-	}
+	
+	};
 
 })(jQuery);
